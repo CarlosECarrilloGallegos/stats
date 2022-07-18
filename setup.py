fd9a84e7b2fbf878283212b2312454f9a31a223d
Carlos Carrillo-Gallegos <90050144+CarlosECarrilloGallegos@users.noreply.github.com>
2022-07-18 15:50:17 -0500

    Structuring python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="ccgstat",
+    version="0.1",
+    author="Carlos Carrillo-Gallegos",
+    author_email="carlos.carrillo-gallegos@yale.edu",
+    description="Stat package by Carlos Carrillo-Gallegos",
+    packages=["ccgstat","ccgstat/Functions"]
+
+)
